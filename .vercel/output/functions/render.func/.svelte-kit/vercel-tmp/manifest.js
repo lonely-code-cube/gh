559595470b56db431dd092ebc81343a3da19f801
Lonely-code-cube <lonely-code-cube@github.com>
2022-10-09 11:18:34 +0530

    Config

---
+++ .vercel/output/functions/render.func/.svelte-kit/vercel-tmp/manifest.js
@@ -0,0 +1,27 @@
+export const manifest = {
+	appDir: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"_app/immutable/start-bbb12ba8.js","imports":["_app/immutable/start-bbb12ba8.js","_app/immutable/chunks/index-4f5b2732.js","_app/immutable/chunks/singletons-93f6bf02.js"],"stylesheets":[]},
+		nodes: [
+			() => import('../output/server/nodes/0.js'),
+			() => import('../output/server/nodes/1.js'),
+			() => import('../output/server/nodes/2.js')
+		],
+		routes: [
+			{
+				id: "",
+				pattern: /^\/$/,
+				names: [],
+				types: [],
+				page: { layouts: [0], errors: [1], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+};
---
+++ .vercel/output/static/_app/immutable/chunks/0-3d99fc9a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-5787f024.js";export{t as component};
